e431dc95eac28d8e47af2adde7e51529d5e825f0
ashishpaudel123 <ashishpaudel206058@gmail.com>
2026-08-18 22:10:35 +0545

    login

--- admin-panel/.next/types/app/layout.ts
+++ admin-panel/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /workspace/admin-panel/src/app/layout.tsx
+// File: /Users/ashishpaudel/Documents/work/hotel/admin-panel/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
